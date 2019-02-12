464ba392e29a55e1b5a43d40c4f96369b7d79308
Navas07 <47551363+Navas07@users.noreply.github.com>
2019-02-12 12:16:48 +0530

    Create Alphabetornot.c

---
+++ Alphabetornot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+   
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("Alphabet.",c);
+    else
+        printf("alphabet.",c);
+
+    return 0;
+}
