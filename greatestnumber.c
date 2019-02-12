3406a5dfa7465e4faff224fdb5fd72a7d2c4d84e
Navas07 <47551363+Navas07@users.noreply.github.com>
2019-02-12 13:52:34 +0530

    Update greatestnumber.c

--- greatestnumber.c
+++ greatestnumber.c
@@ -4,17 +4,17 @@ int main()
 { 
     int A, B, C; 
   
-    printf("Enter the numbers A, B and C: "); 
+   
     scanf("%d %d %d", &A, &B, &C); 
   
     if (A >= B && A >= C) 
-        printf("%d is the largest number.", A); 
+        printf("%d", A); 
   
     if (B >= A && B >= C) 
-        printf("%d is the largest number.", B); 
+        printf("%d",B); 
   
     if (C >= A && C >= B) 
-        printf("%d is the largest number.", C); 
+        printf("%d", C); 
   
     return 0; 
 } 
