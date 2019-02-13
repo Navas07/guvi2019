f7fbba5b0e446cd65efcc59536941380f6da0f21
Navas07 <47551363+Navas07@users.noreply.github.com>
2019-02-13 11:56:47 +0530

    Create lon.c

---
+++ lon.c
@@ -0,0 +1,23 @@
+{
+    int year;
+
+    scanf("%d",&year);
+
+    if(year%4 == 0)
+    {
+        if( year%100 == 0)
+        {
+            
+            if ( year%400 == 0)
+                printf("yes", year);
+            else
+                printf("yes", year);
+        }
+        else
+            printf("yes", year );
+    }
+    else
+        printf("yes", year);
+    
+    return 0;
+}
